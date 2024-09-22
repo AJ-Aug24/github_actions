a741efa7266611226513f0863352d8d63b2116bf
Ubuntu <ubuntu@ip-172-31-4-206.eu-west-1.compute.internal>
2024-09-22 17:56:37 +0000

    Fix flake8 style issues

--- file_test.py
+++ file_test.py
@@ -1,26 +1,6 @@
 # file_test.py
-import pytest
 
-def test_calc_addition():
-    # Test the output of 2 + 4
-    output = 2 + 4
-    assert output == 6
-
-def test_calc_subtraction():
-    # Test the output of 2 - 4
-    output = 2 - 4
-    assert output == -2
-
-def test_calc_multiply():
-    # Test the output of 2 * 4
-    output = 2 * 4
-    assert output == 8
-
-def test_hello():
-    # Test if the output returns 'hello'
-    output = 'hello'
-    assert output == 'hello'
-# file_test.py
+import pytest  # Keep this if you're using pytest; remove if not needed
 
 def test_calc_addition():
     # Test the output of 2 + 4
